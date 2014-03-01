08816c3d910546cf1c776f6350915920dc5a87a2
RiskyAki <aki_afterimage_sepia@men.com>
2014-03-01 15:29:46 +0900

    bundle exec rails new .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AkiChannel::Application.config.secret_key_base = '22d0178abfcbb053465c74b756aa94b212581e7145ec8f8ff4a05e28ea701eb39cdc86428552c5dd60eb71123e1433a6c96f424f036132c27c2465263b6729f0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AkiChannel::Application.config.session_store :cookie_store, key: '_aki-channel_session'
